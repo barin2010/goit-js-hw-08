34a6f594f44224ed7ae4bac94e3d0689bcce7e22
barin2010 <uyrirudenko@gmail.com>
2023-10-23 00:12:18 +0300

    redy task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+  )
+  .join("");
+
+gallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    lightbox.open();
+  }
+});
