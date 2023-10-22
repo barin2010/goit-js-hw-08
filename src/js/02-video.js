67911b3dddd635c2ff5612313082161735de6759
barin2010 <uyrirudenko@gmail.com>
2023-10-23 00:34:32 +0300

    redy task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
